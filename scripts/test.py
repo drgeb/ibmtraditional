2ddc9895fb82f5c6ea18895192aecec42b391572
drgeb <github@drgeb.com>
2018-11-10 19:41:28 -0600

    latest

--- scripts/test.py
+++ scripts/test.py
@@ -3,10 +3,9 @@
 import yaml
 try:
     my_dict = yaml.load(open('example.yaml'))
-
-    nodeName serverName templateID jdbcProvider implementationClass
-
-attributes = sys.argv[5]
+#    nodeName serverName templateID jdbcProvider implementationClass
     print(my_dict)
+    for key in my_dict:
+        print (key)
 except yaml.YAMLError as exc:
     print(exc)
